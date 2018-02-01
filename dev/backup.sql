90e7015864985723c267bcb31f4ddcd567b46266
stack72 <public@paulstack.co.uk>
2018-02-01 16:02:56 +0200

    sedding the database with some templates data for development work

---
+++ dev/backup.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO tsg_templates (id, "name", package, image_id, account_id, firewall_enabled, networks, metadata, userdata, tags, archived) VALUES
+	(319209784155176962, 'test-template-1', 'test-package', 'test-image-updated', 'joyent', false, 'daeb93a2-532e-4bd4-8788-b6b30f10ac17', NULL, 'bash script here', NULL, false),
+	(319209801539354626, 'test-template-2', 'test-package', 'test-image', 'joyent', false, 'daeb93a2-532e-4bd4-8788-b6b30f10ac17', NULL, 'bash script here', NULL, false),
+	(319209812150321154, 'test-template-3', 'test-package', 'test-image', 'joyent', false, 'daeb93a2-532e-4bd4-8788-b6b30f10ac17', NULL, 'bash script here', NULL, false),
+	(319209821014392834, 'test-template-4', 'test-package', 'test-image', 'joyent', false, 'daeb93a2-532e-4bd4-8788-b6b30f10ac17', NULL, 'bash script here', NULL, false),
+	(319209831565656066, 'test-template-5', 'test-package', 'test-image', 'joyent', false, 'daeb93a2-532e-4bd4-8788-b6b30f10ac17', NULL, 'bash script here', NULL, false),
+	(319209841670782978, 'test-template-6', 'test-package', 'test-image', 'joyent', false, 'daeb93a2-532e-4bd4-8788-b6b30f10ac17', NULL, 'bash script here', NULL, false);
