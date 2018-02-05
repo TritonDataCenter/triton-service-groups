bfa6247d5cbed3d91189d3509082c41e9186d3e3
stack72 <public@paulstack.co.uk>
2018-02-05 17:45:23 +0200

    database: Add seeding data for the tsg_groups table
     Please enter the commit message for your changes. Lines starting

--- dev/backup.sql
+++ dev/backup.sql
@@ -6,3 +6,11 @@ INSERT INTO tsg_templates (id, "name", package, image_id, account_id, firewall_e
 	(319209821014392834, 'test-template-4', 'test-package', 'test-image', 'joyent', false, 'daeb93a2-532e-4bd4-8788-b6b30f10ac17', NULL, 'bash script here', NULL, false),
 	(319209831565656066, 'test-template-5', 'test-package', 'test-image', 'joyent', false, 'daeb93a2-532e-4bd4-8788-b6b30f10ac17', NULL, 'bash script here', NULL, false),
 	(319209841670782978, 'test-template-6', 'test-package', 'test-image', 'joyent', false, 'daeb93a2-532e-4bd4-8788-b6b30f10ac17', NULL, 'bash script here', NULL, false);
+
+INSERT INTO tsg_groups (id, "name", template_id, account_id, capacity, datacenter, health_check_interval, instance_tags, archived) VALUES
+	(320376470673326082, 'test-group-1', 319209784155176962, 'joyent', 3, 'us-sw-1,us-east-1', 300, NULL, false),
+	(320376528846356482, 'test-group-2', 319209801539354626, 'joyent', 3, 'us-west-1,us-east-2', 300, NULL, false),
+	(320377354180919298, 'test-group-3', 319209812150321154, 'joyent', 6, 'us-east-1', 60, NULL, false),
+	(320377452358238210, 'test-group-4', 319209821014392834, 'joyent', 1, 'us-east-1,eu-ams-1', 300, NULL, false),
+	(320377513666019330, 'test-group-5', 319209831565656066, 'joyent', 3, 'eu-ams-1', 120, NULL, false),
+	(320377641294168066, 'test-group-6', 319209841670782978, 'joyent', 12, 'us-east-3', 300, NULL, false);
--- dev/setup_db.sh
+++ dev/setup_db.sh
@@ -7,7 +7,7 @@ HOSTPARAMS="--host roach-one --insecure"
 SQL="/cockroach/cockroach.sh sql $HOSTPARAMS"
 
 $SQL -e "CREATE DATABASE IF NOT EXISTS triton;"
-$SQL -d triton -e "CREATE TABLE IF NOT EXISTS tsg_templates (id SERIAL PRIMARY KEY, name TEXT UNIQUE NOT NULL, package TEXT NOT NULL, image_id TEXT NOT NULL, account_id TEXT NOT NULL, firewall_enabled BOOL DEFAULT false, networks TEXT, metadata TEXT, userdata TEXT, tags TEXT, archived BOOL DEFAULT false);"
-$SQL -d triton -e "CREATE TABLE IF NOT EXISTS tsg_groups (id SERIAL PRIMARY KEY, name TEXT UNIQUE NOT NULL, template_id INT NOT NULL REFERENCES tsg_templates (id), account_id TEXT NOT NULL, capacity INT NOT NULL, datacenter TEXT NOT NULL, health_check_interval INT DEFAULT 300, instance_tags TEXT, archived BOOL DEFAULT false);"
+$SQL -d triton -e "CREATE TABLE IF NOT EXISTS tsg_templates (id SERIAL PRIMARY KEY, name TEXT NOT NULL, package TEXT NOT NULL, image_id TEXT NOT NULL, account_id TEXT NOT NULL, firewall_enabled BOOL DEFAULT false, networks TEXT, metadata TEXT, userdata TEXT, tags TEXT, archived BOOL DEFAULT false);"
+$SQL -d triton -e "CREATE TABLE IF NOT EXISTS tsg_groups (id SERIAL PRIMARY KEY, name TEXT NOT NULL, template_id INT NOT NULL REFERENCES tsg_templates (id), account_id TEXT NOT NULL, capacity INT NOT NULL, datacenter TEXT NOT NULL, health_check_interval INT DEFAULT 300, instance_tags TEXT, archived BOOL DEFAULT false);"
 
 /cockroach/cockroach.sh sql $HOSTPARAMS --database=triton < /dev/backup.sql
