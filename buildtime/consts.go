a8c01323755c6e00f0a27ea7f8d5353ffa581671
Justin Reagor <justin.reagor@joyent.com>
2018-03-13 12:12:48 -0400

    Add buildtime package

---
+++ buildtime/consts.go
@@ -0,0 +1,15 @@
+package buildtime
+
+var (
+	Version    string
+	BuildDate  string
+	DocsDate   string
+	GitCommit  string
+	GitBranch  string
+	GitState   string
+	GitSummary string
+)
+
+const (
+	PROGNAME = "triton-sg"
+)
