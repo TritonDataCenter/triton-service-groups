7cebc327d0cf5bf515ce5ecf77edb2eacc791c09
Justin Reagor <justin.reagor@joyent.com>
2018-03-15 17:32:06 -0400

    handlers: authSession => auth.Session

--- server/handlers/auth.go
+++ server/handlers/auth.go
@@ -3,36 +3,38 @@ package handlers
 import (
 	"context"
 	"net/http"
+
+	"github.com/joyent/triton-service-groups/server/handlers/auth"
 )
 
+// authHandler encapsulates the authentication HTTP handler itself. We pipe all
+// active HTTP requests through this object's ServeHTTP method.
 type authHandler struct {
 	handler http.Handler
 }
 
+// AuthHandler constructs and returns the HTTP handler object responsible for
+// authenticating a request. This accepts a chain of HTTP handlers.
 func AuthHandler(handler http.Handler) authHandler {
 	return authHandler{
 		handler: handler,
 	}
 }
 
-type authSession struct {
-	AccountID string
-}
-
-func (a authSession) IsAuthenticated() bool {
-	return a.AccountID != ""
-}
-
-func GetAuthSession(ctx context.Context) authSession {
-	if session, ok := ctx.Value(authKey).(authSession); ok {
+// GetAuthSession pulls the current active authenticated session out of the
+// current request context. This keeps authentication scoped to the active
+// request only.
+func GetAuthSession(ctx context.Context) auth.Session {
+	if session, ok := ctx.Value(authKey).(auth.Session); ok {
 		return session
 	}
-	return authSession{}
+	return auth.Session{}
 }
 
+// ServeHTTP serves HTTP requests through the authentication process scoped to
+// whatever pre-defined data we need accessible through the authHandler struct.
 func (a authHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	// TODO(justinwr): Actually authenticate the request against something.
-	session := authSession{
+	session := auth.Session{
 		AccountID: "joyent",
 	}
 
