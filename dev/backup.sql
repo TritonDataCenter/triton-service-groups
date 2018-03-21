deb25d05f019abf7bd1d0f61e4305d4bfe573c46
Justin Reagor <justin.reagor@joyent.com>
2018-03-20 21:25:43 -0400

    makefile: Add more database utilities for dev

--- dev/backup.sql
+++ dev/backup.sql
@@ -2,6 +2,7 @@ SET sql_safe_updates = false;
 
 DELETE FROM tsg_groups;
 DELETE FROM tsg_templates;
+DELETE FROM tsg_keys;
 DELETE FROM tsg_users;
 DELETE FROM tsg_accounts;
 
---
+++ dev/drop.sql
@@ -0,0 +1,7 @@
+SET sql_safe_updates = false;
+
+DROP TABLE IF EXISTS tsg_groups;
+DROP TABLE IF EXISTS tsg_templates;
+DROP TABLE IF EXISTS tsg_users;
+DROP TABLE IF EXISTS tsg_accounts;
+DROP TABLE IF EXISTS tsg_keys;
